d4c7882e526956a5b11d22b7f29801fa6c09fe1f
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-07-24 23:32:28 +0530

    Create twoSum.java

---
+++ twoSum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int[] ans = new int[2];
+
+		// number and its corresponding index
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (int index = 0; index < nums.length; index++) {
+
+			if (!map.containsKey(nums[index])) {
+
+				map.put(target - nums[index], index);
+
+			} else {
+				ans[0] = index;
+				ans[1] = map.get(nums[index]);
+				break;
+			}
+		}
+		return ans;
+    }
+}
