1e38df1d631f0949dfff296d564faf766e3e41e4
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-07-27 17:35:29 +0530

    Create PlusOne.java

---
+++ PlusOne.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int newnum[]=new int[n+1];
+        newnum[0]=1;
+        return newnum;
+    }
+}
