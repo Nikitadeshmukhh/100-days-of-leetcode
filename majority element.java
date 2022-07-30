614ed9069c0e0ca5ef864133eec5d6ab5a522f0b
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-07-31 00:31:41 +0530

    Create majority element.java

---
+++ majority element.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       int result=0,count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(count==0)
+            {result=nums[i];
+             count=1;
+            }
+            else if(result==nums[i])
+            {
+                count++;
+            }
+            else{
+                count--;
+            } 
+        }
+        return result;
+    }
+}
+
+class Solution {
+    public int majorityElement(int[] nums) {
+         HashMap<Integer,Integer> map = new HashMap<>();
+        int ans = 0;
+        for(int i = 0; i< nums.length; i++){
+            if(!map.containsKey(nums[i])) map.put(nums[i], 1);
+            else map.put(nums[i], map.get(nums[i])+1);
+            if( map.get(nums[i]) > nums.length/2) ans = nums[i];
+        }
+        return ans;
+    }
+}
