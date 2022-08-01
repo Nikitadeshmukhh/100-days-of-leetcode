60c9e2729d01ab642a9cb4044a3bcb2b1161a0dc
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-08-01 20:20:40 +0530

    Update Kth largest element.java

--- Kth largest element.java
+++ Kth largest element.java
@@ -1,16 +1,22 @@
+//We can solve this using priority queue or by sorting algorithm
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-     // PriorityQueue<Integer>pq=new PriorityQueue<>(k+1);
-     //    for(int i:nums)
-     //    {
-     //        pq.add(i);
-     //        if(pq.size()>k)
-     //        {
-     //            pq.poll();
-     //        }
-     //    }
-     //    return pq.poll();
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+     PriorityQueue<Integer>pq=new PriorityQueue<>(k+1);
+        for(int i:nums)
+        {
+            pq.add(i);
+            if(pq.size()>k)
+            {
+                pq.poll();
+            }
+        }
+        return pq.poll();
+    }
+}
+//By using sorting technique
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+         Arrays.sort(nums);
+         return nums[nums.length-k];
     }
 }
