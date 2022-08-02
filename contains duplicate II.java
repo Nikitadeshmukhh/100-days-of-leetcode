9c89837bd37b8676b6a5db9c08265259c054ada9
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-08-03 01:09:08 +0530

    Create contains duplicate II.java

---
+++ contains duplicate II.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int current=nums[i];
+            if(map.containsKey(current)&&i-map.get(current)<=k)
+                return true;
+            else
+                map.put(current,i);
+        }
+        return false;
+    }
+}
