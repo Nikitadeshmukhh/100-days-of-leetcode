20ad448fc9346dc3f1ec43b1ad1bd94e45964511
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-08-03 22:39:58 +0530

    Create summary ranges.java

---
+++ summary ranges.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list= new ArrayList();
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            int start=nums[i];
+            while(i+1<n && nums[i+1]==nums[i]+1)
+                i++;
+            if(start!=nums[i])
+                list.add(""+start+"->"+nums[i]);
+            else
+                list.add(""+start);
+        }
+        return list;
+    }
+}
