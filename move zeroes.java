34143b986c2f599bf1bc6fc24df018d969816e58
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-08-04 20:04:27 +0530

    Create move zeroes.java

---
+++ move zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int n = nums.length;
+        int i = 0, j = 0;
+        while(j<n){
+            if(nums[j]==0){
+                j++;
+            }
+            else{
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+                j++;
+            }
+        }
+    }
+}
