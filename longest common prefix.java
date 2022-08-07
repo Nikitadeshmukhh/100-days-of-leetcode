3c5da3576e39f7f4badad76efe790bc111e23d68
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-08-07 21:12:36 +0530

    Update longest common prefix.java

--- longest common prefix.java
+++ longest common prefix.java
@@ -29,3 +29,26 @@ class Solution {
     return strs[0].substring(0, i);
 }
 }
+//Another optimised solution can be:-
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length ==0){
+        return "";
+    }
+    if(strs.length == 1){
+        return strs[0];
+    }
+   String prefix=strs[0];
+   for(int i=1;i<strs.length;i++)
+   {
+       while(strs[i].indexOf(prefix)!=0)
+       {
+           prefix=prefix.substring(0,prefix.length()-1);
+           if(prefix.isEmpty()) return "";
+       }
+   }
+        return prefix;
+}
+
+}
