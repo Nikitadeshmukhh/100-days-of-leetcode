68ee35f43c91baafd2cfca422a198d28f823baaa
Nikita Deshmukh <82337730+Nikitadeshmukhh@users.noreply.github.com>
2022-08-31 21:20:47 +0530

    Create running sum of 1D array.java

---
+++ running sum of 1D array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int res[]=new int[nums.length];
+        res[0]=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            res[i]= res[i-1]+nums[i];
+        }
+        return res;
+    }
+}
